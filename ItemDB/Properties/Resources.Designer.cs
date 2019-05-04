6355238882c5b8a342132197a868bdf72f2ce816
Tec <daniel112092@gmail.com>
2019-05-05 00:26:21 +0200

    Push base project files

--- ItemDB/Properties/Resources.Designer.cs
+++ ItemDB/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ItemDB.Presentation.Properties {
+namespace ItemDB.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ItemDB.Presentation.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ItemDB.Presentation.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ItemDB.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- ItemDB/Properties/Settings.Designer.cs
+++ ItemDB/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ItemDB.Presentation.Properties {
+namespace ItemDB.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
