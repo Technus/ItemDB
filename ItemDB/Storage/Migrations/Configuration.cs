e040717ba7c20ed86acea563ea28f4feb2b8029c
Tec <daniel112092@gmail.com>
2019-05-05 10:17:32 +0200

    Seed Locations Table

--- ItemDB/Storage/Migrations/Configuration.cs
+++ ItemDB/Storage/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace ItemDB.Storage.Migrations
 {
+    using ItemDB.Core.Model;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,10 +16,17 @@ namespace ItemDB.Storage.Migrations
 
         protected override void Seed(ItemDbStorage context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            context.Locations.AddOrUpdate(l=>l.Name,new Location { Name = "Room A" });
+            context.SaveChanges();
+            context.Locations.AddOrUpdate(l => l.Name, 
+                new Location { Name = "Shelf A", ParentLocationId = context.Locations.First(l => l.Name == "Room A").Id },
+                new Location { Name = "Shelf B" },
+                new Location { Name = "Shelf C" });
+            context.SaveChanges();
+            context.Locations.AddOrUpdate(l => l.Name, 
+                new Location { Name = "Box A", ParentLocationId = context.Locations.First(l => l.Name == "Shelf A").Id },
+                new Location { Name = "Box B", ParentLocationId = context.Locations.First(l => l.Name == "Shelf A").Id },
+                new Location { Name = "Box C", ParentLocationId = context.Locations.First(l => l.Name == "Shelf C").Id });
         }
     }
 }
