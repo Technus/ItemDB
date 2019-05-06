4b0c6875dbfa0af11dc081dc89aad1db361fe195
Tec <daniel112092@gmail.com>
2019-05-06 07:03:18 +0200

    Initial state of Contacts View and ViewModel

---
+++ ItemDB/Presentation/ViewModel/ContactsView.cs
@@ -0,0 +1,14 @@
+﻿using ItemDB.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemDB.Presentation.ViewModel
+{
+    class ContactsView
+    {
+        public List<Contact> 
+    }
+}
